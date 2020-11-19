ed4f8d9f9b1b1a03615a4b5eecfa2e41277056e9
Stefaniya Yanovich <stefanishy@gmail.com>
2020-11-19 20:16:47 +0300

    Добавила первое дз

---
+++ lesson01/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let money; 
+let income; 
+let addExpenses; 
+let deposit; 
+let mission; 
+let period;
+
+document.getElementById('button').addEventListener("click", () => {
+     alert("Один важный секрет: нужно идти туда, куда хочется, а не туда, куда якобы надо");
+ });
+ console.log('Когда человек перестает что-либо понимать, он на верном пути.');
