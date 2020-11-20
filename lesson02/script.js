f985c17c4319280d0da0bdee71a48e8324048af5
Stefaniya Yanovich <stefanishy@gmail.com>
2020-11-20 12:59:18 +0300

    добавила дз2

---
+++ lesson02/script.js
@@ -0,0 +1,21 @@
+const money = 900; 
+const income = '200'; 
+const addExpenses = 'аренда, интернет, пиво, кошачий корм'; 
+const deposit = true; 
+const mission = 20000; 
+const period = 12;
+const budgetDay = money/30;
+
+
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать - ${mission}$`);
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay);
+
+
+
