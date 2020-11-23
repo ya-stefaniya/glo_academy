be5a68f701a49dcff841b76aec976aeac5f53b2d
Stefaniya Yanovich <stefanishy@gmail.com>
2020-11-23 18:20:06 +0300

    удаление ненужные файлы

--- lesson02-hard/script.js
+++
@@ -1,8 +0,0 @@
-const num = 266219;
-
-calcNum = num => {
- const multipliedNum = num.toString().split('').reduce((sum,i)=>sum*i)**3;
- return +multipliedNum.toString().substr(0,2);
-};
-
-console.log(calcNum(num));
