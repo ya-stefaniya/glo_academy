727bcf6f60936a09d39a754c3d1e94367a339c1c
Stefaniya Yanovich <stefanishy@gmail.com>
2020-12-14 21:58:47 +0300

    консоли

---
+++ lesson18/extra/index.js
@@ -0,0 +1,10 @@
+const text = document.querySelector('.text');
+const result = document.querySelector('.result');
+
+
+text.addEventListener('change', ()=>{
+    console.log('change');
+    result.addEventListener('input', ()=>{
+        onsole.log('input');
+
+})
